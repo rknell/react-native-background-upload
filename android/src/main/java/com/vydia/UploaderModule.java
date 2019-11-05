cd09990708fdd139b9da532de2715aa1b843ffd2
Ryan Knell <ryan.knell@gmail.com>
2019-11-05 15:22:00 +1000

    Update UploaderModule.java
    Update for AndroidX

--- android/src/main/java/com/vydia/UploaderModule.java
+++ android/src/main/java/com/vydia/UploaderModule.java
@@ -2,7 +2,7 @@ package com.vydia.RNUploader;
 
 import android.content.Context;
 import android.os.Build;
-import android.support.annotation.Nullable;
+import androidx.support.annotation.Nullable;
 import android.util.Log;
 import android.webkit.MimeTypeMap;
 
